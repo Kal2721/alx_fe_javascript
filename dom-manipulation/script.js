5f8305cf071be90771e3e8caee8250176244459f
kalkidan <kalkidanteshale6@gmail.com>
2025-06-26 12:52:15 +0300

    Creating a page to display a random quote

---
+++ dom-manipulation/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () =>{
+	const quote-display = document.getElementById("quoteDisplay");
+	const newQuote = document.getElementById("newQuote");
+
+	const quoteObj = [
+		{text: "We loved with a love that was more than love.", category: "love"},
+		{text: "Love is the beauty of the soul.", category: "love"},
+		{text: "To love is to burn, to be on fire.", category: "love"},
+		{text: "Believe you can, and you're halfway there", category: "inspirational"},
+		{text: "Every day is a fresh start", category: "inspirational"},
+		{text: "You are stronger than you think.", category: "inspirational"},
+		{text: "Don't take life too seriously. You will never get out of it alive.", category: "humorous"},
+		{text: "The man who says his wife can't take a joke, forgets that she took him.", category: "humorous"},
+		{text: "Life is a succession of lessons which must be lived to be understood", category: "life"},
+		{text: "Keep your face always toward the sunshine—and shadows will fall behind you.", category: "life"}
+	];
+
+	function showRandomQuote(){
+		const randomIndex = Math.floor(Math.random() * quoteObj.length);
+		const randomObject = quoteObj[randomIndex];
+
+		quote-display.textcontent = `"$quoteObj.text" - $quoteObj.category`;
+	}
+
+	newQuote.addEventListener("click", () => {
+		showRandomQuote();
+	});
+
+	function createAddQuoteForm(){
+		x
+	}
+
+});
